89398a20a0d0e40eafe938ea3afa6c003f7e54bf
Joe Marriott <marriott90@gmail.com>
2017-03-14 15:34:02 +0000

    game and README added

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	public int maxGuessesAvailable = 10;
+
+	public Text text;
+
+	void Start () {
+		StartGame();
+	}
+
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		NextGuess();
+	}
+
+	public void GuessHigher (){
+		min = guess;
+		NextGuess();
+	}
+
+	public void GuessLower (){
+		max = guess;
+		NextGuess();
+	}
+
+	void NextGuess ()
+	{
+		guess = Random.Range(min, max+1);
+		text.text = guess.ToString();
+		maxGuessesAvailable -= 1;
+		if (maxGuessesAvailable <= 0) {
+			SceneManager.LoadScene("Win");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+##Number Wizard
+***
+
+Game created using C# and Unity. 
+
+Research for Makers Academy final project
+
+https://www.udemy.com/unitycourse/learn/v4/
+
+<img src="http://i.imgur.com/yiWXGCo.png">
+<img src="http://i.imgur.com/VNDL9wf.png">
+<img src="http://i.imgur.com/JWMsTTL.png">
+<img src="http://i.imgur.com/r977kW2.png">
